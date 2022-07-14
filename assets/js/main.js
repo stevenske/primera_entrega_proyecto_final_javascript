b53403e8bbcd3c5a07366d54cf7e459700828edc
esteban vargas <esteban.var@outlook.com>
2022-07-13 23:17:55 -0300

    add poll prompt with objects

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// PRIMERA PARTE ENTREGA PROYECTO FINAL
 let price = 0
 let discount = 0
 let name = prompt("hi!! what's your name?")
@@ -13,24 +14,37 @@ function discount10(a, b) {
 }
 
 
+
+class country{
+    constructor(countryName,price){
+        this.countryName = countryName
+        this.price = price
+    }
+}
+
+
+const country1 = new country('spain',1800)
+const country2 = new country('france',2200)
+const country3 = new country('england',2100)
+
+
+
+
 while (trip != 'accept') {
     switch (trip) {
-        case 'spain':
-            price = 1800
-            discount = discount10(price, 0.10)
-            alert(`the price of the trip to ${trip} is $${price} and with the 10% discount the total is ${discount}`)
+        case country1.countryName:
+            discount = discount10(country1.price, 0.10)
+            alert(`the price of the trip to ${trip} is $${country1.price} and with the 10% discount the total is ${discount}`)
             trip = prompt("write accept for confirm the trip!!").toLowerCase()
             break
-        case 'france':
-            price = 2200
-            discount = discount10(price, 0.10)
-            alert(`the price of the trip to ${trip} is $${price} and with the 10% discount the total is ${discount}`)
+        case country2.countryName:
+            discount = discount10(country2.price, 0.10)
+            alert(`the price of the trip to ${trip} is $${country2.price} and with the 10% discount the total is ${discount}`)
             trip = prompt("write accept for confirm the trip!!").toLowerCase()
             break
-        case 'england':
-            price = 2100
-            discount = discount10(price, 0.10)
-            alert(`the price of the trip to ${trip} is $${price} and with the 10% discount the total is ${discount}`)
+        case country3.countryName:
+            discount = discount10(country3.price, 0.10)
+            alert(`the price of the trip to ${trip} is $${country3.price} and with the 10% discount the total is ${discount}`)
             trip = prompt("write accept for confirm the trip!!").toLowerCase()
             break
         case '':
@@ -63,3 +77,35 @@ function pay() {
 }
 
 pay()
+// SEGUNDA PARTE ENTREGA PROYECTO FINAL
+
+alert(`${name} we are carrying out a poll to our clients to find out where they would like to travel`)
+
+class wishCountry {
+    constructor(newCountry) {
+        this.newCountry = newCountry
+    }
+}
+
+let newCountryName
+const countries = []
+let newCountry = prompt('what country do you want to trabel with Traintips?')
+newCountryName = countries.push(new wishCountry(newCountry))
+
+let confirmar = prompt('do you want to add a few more? (yes/no)')
+
+if (confirmar == 'yes') {
+
+    while (newCountry != 'esc') {
+        newCountry = prompt('what country do you want to trabel with Traintrips? (write "esc" if you write all the countries that you want)').toLowerCase()
+        if(newCountry!="esc"){
+            newCountryName = new wishCountry(newCountry)
+            countries.push(newCountryName)
+        }
+    }
+    // countries.splice(countries.length - 1, 1)
+}
+alert('THANKS YOU SO MUCH FOR COMPLETE THIS POLL')
+
+console.log(countries)
+
